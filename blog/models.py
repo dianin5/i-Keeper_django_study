e954fc92fd1219d32e799a67ef21e260d28f6a2a
dianin5 <xyg1940@cu.ac.kr>
2024-05-08 23:02:19 +0900

    admin 설정 이후 post 2개 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# 모델을 정의하는 코드입니다. 
+# models.Model -> Post가 장고 모델임을 밝힙니다. 
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+    
+'''
+- models.CharField
+    - 글자 수 제한 / max_length = 200
+- models.TextField
+    - 글자 수 제한이 없는 긴 텍스트를 위한 속성입니다.
+- models.DateTimeField
+    - 날짜와 시간을 의미합니다.
+- models.Foreignkey
+    - 다른 모델에 대한 링크를 의미합니다.
+
+- def publish(self):
+    - publish 라는 메서드입니다.
+    - published_date 값을 업데이트하고, 저장하는 역할을 수행합니다.
+    - (글 공개)
+
+- __str__(self):
+    - 호출시 Post 모델의 제목 테스트 / String 제공
+    '''
