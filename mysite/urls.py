72d8befbcba81ae26b515f0b073cc9178b232dc4
dianin5 <xyg1940@cu.ac.kr>
2024-05-08 23:34:11 +0900

    PART1 종료.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 # urlresolver가 사용하는 패턴 목록을 포함합니다.
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
