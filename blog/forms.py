8f3a844ab1349172c85dc8ec8a8598eacad62da2
dianin5 <xyg1940@cu.ac.kr>
2024-05-21 15:36:47 +0900

    2주차 실습 완료, 로그인, 로그아웃, 아이콘 추가(현재 /static/css/blog.css HTTP/1.1 404 응답 나옴)

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+
+class CommentForm(forms.ModelForm): # CommentForm 클래스 추가 
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
 
 class PostForm(forms.ModelForm):
 
--- blog/models.py
+++ blog/models.py
@@ -41,3 +41,19 @@ class Post(models.Model):
 - __str__(self):
     - 호출시 Post 모델의 제목 테스트 / String 제공
     '''
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
